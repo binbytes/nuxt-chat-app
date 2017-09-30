32fd73bb4d79e05634986343cfb0598c97be0926
Nikunj Kanetiya <kanetiya@gmail.com>
2017-09-30 11:09:42 +0530

    Add milligram css

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,18 @@ module.exports = {
   ** Headers
   ** Common headers are already provided by @nuxtjs/pwa preset
   */
-  head: {},
+  head: {
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
+    ]
+  },
   /*
   ** Customize the progress-bar color
   */
   loading: { color: '#3B8070' },
+  css: [
+    './node_modules/milligram/dist/milligram.min.css'
+  ],
   /*
   ** Customize app manifest
   */
