d3a894ce244ff0a42a8b9027cd6aa3125f6ff990
Nikunj Kanetiya <kanetiya@gmail.com>
2017-09-30 11:58:57 +0530

    Initial layout

---
+++ pages/chats.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <div class="row">
+      <aside class="column column-25 users-list">
+        Users List
+      </aside>
+      <div class="column column-75 chat-section">
+        Chat View
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+// Curerntly we will not put logic, just bulit layout first
+export default {
+  name: 'chat-view',
+  data() {
+    return {
+      users: []
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,101 +1,37 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        NUXT
-      </h1>
-      <h2 class="subtitle">
-        PWA Vue.js Application
-      </h2>
-      <div :class="['network',online ? 'online' : 'offline']">
-        <div class="circle"></div>
-        {{ online ? 'online' : 'offline' }}
-      </div>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green" rel="noopener">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey" rel="noopener">GitHub</a>
-      </div>
+    <div class="row">
+      <form v-on:submit.prevent="onSubmit" class="column column-50 column-offset-25">
+        <h3>Login</h3>
+        <fieldset>
+          <label for="nameField">Username</label>
+          <input type="text" placeholder="Username" v-model="username">
+
+          <label for="nameField">Password</label>
+          <input type="text" placeholder="Password" v-model="password">
+
+          <input class="button-primary" type="submit" value="Login">
+        </fieldset>
+      </form>
     </div>
   </section>
 </template>
 
 <script>
-  import Logo from '~/components/Logo.vue'
-
-  export default {
-    components: {Logo},
-    data () {
-      return {
-        online: true
-      }
-    },
-    mounted () {
-      if (!window.navigator) {
-        this.online = false
-        return
-      }
-      this.online = Boolean(window.navigator.onLine)
-      window.addEventListener('offline', this._toggleNetworkStatus)
-      window.addEventListener('online', this._toggleNetworkStatus)
-    },
-    methods: {
-      _toggleNetworkStatus ({ type }) {
-        this.online = type === 'online'
-      }
-    },
-    destroyed () {
-      window.removeEventListener('offline', this._toggleNetworkStatus)
-      window.removeEventListener('online', this._toggleNetworkStatus)
+// Curerntly we will not put logic, just bulit layout first
+export default {
+  name: 'login',
+  data() {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    onSubmit( ) {
+      // Put login logic & middleware call here
+      this.$router.replace({ path: 'chats' })
     }
   }
+}
 </script>
-
-<style>
-  .container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
-
-  .title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
-  }
-
-  .network {
-    font-weight: 400;
-    font-size: 1rem;
-  }
-
-  .network .circle {
-    display: inline-block;
-    width: 1rem;
-    height: 1rem;
-    background: green;
-    padding: .1rem .5rem;
-    border-radius: 1rem;
-  }
-
-  .network.offline .circle {
-    background: red;
-  }
-</style>
