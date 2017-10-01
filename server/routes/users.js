d432e5859d6d4002fe7e7b1ebaa21da82a00114a
Nikunj Kanetiya <kanetiya@gmail.com>
2017-10-01 17:57:53 +0530

    js beautify

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,12 +14,12 @@ const users = [
 ]
 
 /* GET users listing. */
-router.get('/users', function (req, res, next) {
+router.get('/users', function(req, res, next) {
   res.json(users)
 })
 
 /* GET user by ID. */
-router.get('/users/:id', function (req, res, next) {
+router.get('/users/:id', function(req, res, next) {
   const id = parseInt(req.params.id)
   if (id >= 0 && id < users.length) {
     res.json(users[id])
