757a56eddd5d37df5a1e28254d1667a005387d07
Nikunj Kanetiya <kanetiya@gmail.com>
2017-10-12 11:55:44 +0530

    Remove not unwanted code

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -2,16 +2,4 @@ import io from 'socket.io-client'
 
 const socket = io(process.env.SOCKET_HOST_URL)
 
-// socket.on('new-message', (message) => {
-//   store.dispatch('pushMessage', message)
-// })
-
-// socket.on('online-users', (ids) => {
-//   if (ids) {
-//     setTimeout(() => {
-//       store.dispatch('setOnlineUsers', ids)
-//     }, 1000)
-//   }
-// })
-
 export default socket
