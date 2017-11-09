b584dbc084ff45089a656481620817cdad9e1b4a
Nikunj Kanetiya <kanetiya@gmail.com>
2017-11-09 09:49:58 +0530

    Redesign register form

--- pages/register.vue
+++ pages/register.vue
@@ -1,32 +1,48 @@
 <template>
-  <section class="container auth-page">
-    <div class="row">
-      <div class="column column-50 column-offset-25">
+  <div class="bg-grey-lighter h-screen font-sans">
+
+    <div class="container mx-auto h-full flex justify-center items-center">
+      <div class="w-1/3">
+        <h1 class="font-hairline mb-6 text-center">Register to Chat app</h1>
         <form v-on:submit.prevent="onSubmit">
-          <h3>Register</h3>
-          <fieldset>
-            <div class="error-box" v-text="errors.error" v-if="errors.error">
+          <div class="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg">
+
+            <div class="error-box text-red text-center mb-4" v-if="errors.error" v-text="errors.error"></div>
+
+            <div class="mb-4">
+              <label class="font-bold text-grey-darker block mb-2">Name</label>
+              <input type="text" v-model="name" :class="['block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow', errors.username ? 'border-red mb-3' : '']" placeholder="Your Username">
+              <p v-if="errors.name" class="text-red text-xs italic">Please enter a name.</p>
             </div>
 
-            <label for="name">Name</label>
-            <input type="text" :class="[errors.name ? 'has-error' : null]" placeholder="Name" v-model="name">
-
-            <label for="Username">Username</label>
-            <input type="text" :class="[errors.username ? 'has-error' : null]" placeholder="Username" v-model="username">
-
-            <label for="Password">Password</label>
-            <input type="password" :class="[errors.password ? 'has-error' : null]" placeholder="Password" v-model="password">
-
-            <input class="button-primary" type="submit" value="Register">
-
-            <div>
-              <router-link to="/login">Already have account, login from here</router-link>
+            <div class="mb-4">
+              <label class="font-bold text-grey-darker block mb-2">Username</label>
+              <input type="text" v-model="username" :class="['block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow', errors.username ? 'border-red mb-3' : '']" placeholder="Your Username">
+              <p v-if="errors.username" class="text-red text-xs italic">Please choose a username.</p>
             </div>
-          </fieldset>
+
+            <div class="mb-4">
+              <label class="font-bold text-grey-darker block mb-2">Password</label>
+              <input type="password" v-model="password" :class="['block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow', errors.password ? 'border-red mb-3' : '']" placeholder="Your Password">
+              <p v-if="errors.password" class="text-red text-xs italic">Please choose a password.</p>
+            </div>
+
+            <div class="flex items-center justify-between">
+              <button type="submit" class="bg-teal-dark hover:bg-teal text-white font-bold py-2 px-4 rounded">
+                Register
+              </button>
+            </div>
+
+          </div>
+          <div class="text-center">
+            <p class="text-grey-dark text-sm">Already have account?
+              <router-link to="/login" class="no-underline text-blue font-bold">Login from here.</router-link>
+            </p>
+          </div>
         </form>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
